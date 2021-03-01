2219cd55bf618a29fbbdb232a22e5f4ba245872a
SimMan <liwei0990@gmail.com>
2021-03-01 16:05:55 +0800

    add skeleton

--- lib/src/components/skeleton/skeleton.dart
+++ lib/src/components/skeleton/skeleton.dart
@@ -1,17 +1,3 @@
-/*
- * Copyright (c) 2020 Pawan Kumar. All rights reserved.
- *
- *  * Licensed under the Apache License, Version 2.0 (the "License");
- *  * you may not use this file except in compliance with the License.
- *  * You may obtain a copy of the License at
- *  * http://www.apache.org/licenses/LICENSE-2.0
- *  * Unless required by applicable law or agreed to in writing, software
- *  * distributed under the License is distributed on an "AS IS" BASIS,
- *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  * See the License for the specific language governing permissions and
- *  * limitations under the License.
- */
-
 import 'package:flutter/material.dart';
 
 const int _kDefaultMS = 1000;
@@ -169,8 +155,9 @@ class NeSkeletonState extends State<NeSkeleton>
           final double width = widget.type == NeSkeletonAnimationType.shimmer
               ? widget.width
               : _animation.value;
-          final decColor =
-              widget.type == NeSkeletonAnimationType.stretch ? color : null;
+          // final decColor =
+          //     widget.type == NeSkeletonAnimationType.stretch ? color : null;
+          final decColor = null;
 
           return Container(
             decoration: BoxDecoration(
